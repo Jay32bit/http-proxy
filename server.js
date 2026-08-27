95e2892dceffc41a5425eaa8fbaf8ae9aa47b3dc
Jay32bit <214229740+Jay32bit@users.noreply.github.com>
2026-08-28 06:03:18 +0800

    cleanup

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 
 const app = express();
-
+// Should fallback to port 3000
 const PORT = process.env.PORT || 3000;
+
+// Main target for the proxy
+// the server host should have some sort of "secret" variable that can be used
 const TARGET_BASE = process.env.TARGET_BASE;
 
 if (!TARGET_BASE) {
@@ -11,7 +14,7 @@ if (!TARGET_BASE) {
 }
 
 let baseUrl;
-
+//if its some gibberish url it should catch it here
 try {
   baseUrl = new URL(TARGET_BASE);
 } catch {
@@ -22,10 +25,10 @@ try {
 app.use(async (req, res) => {
   try {
     const target = new URL(req.originalUrl, baseUrl);
-
     const headers = new Headers();
-
     // Forward useful request headers
+    // some api related stuff work via headers
+    // this makes it way easier to not blame the proxy xD
     for (const name of [
       "user-agent",
       "accept",
@@ -34,19 +37,15 @@ app.use(async (req, res) => {
       "authorization"
     ]) {
       const value = req.get(name);
-
       if (value) {
         headers.set(name, value);
       }
     }
-
     const response = await fetch(target, {
       method: req.method,
       headers
     });
-
     res.status(response.status);
-
     // Forward relevant response headers
     for (const name of [
       "content-type",
